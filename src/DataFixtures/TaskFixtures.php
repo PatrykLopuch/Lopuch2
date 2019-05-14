bd4489c3a37b63f7bdefbe0bbb5d1dcf5b8413db
Patryk <patryk.lopuszynski@gmail.com>
2019-05-14 19:54:27 +0200

    pod koniec doctrina

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class TaskFixtures extends AbstractBaseFixtures
+{
+    /**
+     * Faker.
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     *
+     * @var \Doctrine\Common\Persistence\ObjectManager
+     */
+    protected $manager;
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function loadData(ObjectManager $manager)
+    {
+        $this->faker = Factory::create();
+        $this->manager = $manager;
+
+        for ($i = 0; $i < 10; ++$i)
+        {
+            $task = new Task();
+            $task->setTitle($this->faker->sentence);
+            $task->setCreatedAt($this->faker->dateTimeBetween('-100 days','-1 days'));
+            $task->setUpdatedAt($this->faker->dateTimeBetween('-100 days','-1 days'));
+            $this->manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -1,16 +1,24 @@
 <?php
-
+/**
+ * Task Entity
+ */
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * Class Task
+ *
  * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
  * @ORM\Table(name="tasks")
  */
 class Task
 {
     /**
+     * Primary key.
+     *
+     * @var int
+     *
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -32,16 +40,32 @@ class Task
      */
     private $updatedAt;
 
+    /**
+     * Getter for Id
+     *
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Getter for title
+     *
+     * @return string|null
+     */
     public function getTitle(): ?string
     {
         return $this->title;
     }
 
+    /**
+     * Setter for title
+     *
+     * @param string $title
+     * @return Task
+     */
     public function setTitle(string $title): self
     {
         $this->title = $title;
@@ -49,11 +73,23 @@ class Task
         return $this;
     }
 
+    /**
+     * Getter for created at
+     *
+     * @return \DateTimeInterface|null
+     */
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
+    /**
+     *
+     * Setter for created at
+     *
+     * @param \DateTimeInterface $createdAt
+     * @return Task
+     */
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
@@ -61,11 +97,22 @@ class Task
         return $this;
     }
 
+    /**
+     * getter for UpdatedAt
+     *
+     * @return \DateTimeInterface|null
+     */
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updatedAt;
     }
 
+    /**
+     * setter for UpdatedAt
+     *
+     * @param \DateTimeInterface $updatedAt
+     * @return Task
+     */
     public function setUpdatedAt(\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
